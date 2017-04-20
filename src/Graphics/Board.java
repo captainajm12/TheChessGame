22a4ae642a6234aeb4eacb3bd88069ed4ab3b17a
DerVerrater <robertgarrett404@gmail.com>
2017-04-19 21:30:38 -0700

    Fixed the board panel not accepting key input
    The Board object wasn't focusable, and the JFrame was. I swtiched this
    (a listener is still attached to the JFrame, though. Consider removing
    it as it doesn't function)

--- src/Graphics/Board.java
+++ src/Graphics/Board.java
@@ -2,6 +2,8 @@ package Graphics;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -10,20 +12,37 @@ import javax.swing.JPanel;
 public class Board extends JPanel {	//extend there so it uses JPanel crap
 
 	public Board(){
-		
 		super();	//references super class from JPanel
+		
 		this.setLayout(null);	//Disabling layout manager again for everything inside window
 		this.setBackground(Color.BLUE);	//filling in Window with blue
 		this.setBounds(0, 0, 500, 500);	//Setting the bounds how the interior of the window is.
+		this.setFocusable(true);
 		this.requestFocus();	//things inside Window requesting focus
 		this.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent arg0) {//changed to mousePressed because mouseClicked is not fired if the mouse is dragged (pressed & moved)
+				System.out.println("lol fat facee, I can't type.");
+			}
+		});
+		this.addKeyListener(new KeyListener(){
 
-			public void mouseClicked(MouseEvent arg0) {
-			System.out.println("lol fat facee, I can't type.");
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("Key pressed");
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
 				
 			}
 
-
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
 			
 		});
 			
--- src/Graphics/Screen.java
+++ src/Graphics/Screen.java
@@ -1,6 +1,9 @@
 package Graphics;
 
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class Screen {	//Creating window for game
@@ -8,14 +11,35 @@ public class Screen {	//Creating window for game
 	JFrame frame = new JFrame();	//creating instance of frame from JFrame
 	
 	public Screen(){	//Constructor
-	frame.setLayout(null);	//Disabling layout manager
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Setting it so when you click close button it closes everything by default
-	frame.setBounds(0, 0, 500, 500);	//setting bounds of the window
-	frame.add(new Board());	//instance of Board class being created
-	frame.setLocationRelativeTo(null);	//Sets location of window relative to crap
-	frame.setFocusable(true);	//setting it so by default it is focused on
-	frame.setTitle("The Chess game");	//sets title of window
-	frame.setVisible(true);	//making it so one can see the window pop up
+		frame.setLayout(null);	//Disabling layout manager
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Setting it so when you click close button it closes everything by default
+		frame.setBounds(0, 0, 500, 500);	//setting bounds of the window
+		frame.add(new Board());	//instance of Board class being created
+		frame.setLocationRelativeTo(null);	//Sets location of window relative to crap
+		frame.setFocusable(false);	//setting it so by default it is focused on
+		frame.addKeyListener(new KeyListener(){
 
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println("Key pressed");
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		frame.setTitle("The Chess game");	//sets title of window
+		frame.setVisible(true);	//making it so one can see the window pop up
+		
 	}
 }
